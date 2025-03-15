bb44895cc9dc6c2b278b6fae382b77eec79f561f
Vania2007 <momotis2007@gmail.com>
2025-03-15 18:10:56 +0200

    hw-51

---
+++ wp-content/themes/greenshift/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element'), 'version' => '7dc13cba0ae7e66b6783');
---
+++ wp-content/themes/greenshift/build/settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components'), 'version' => '1c3457ca4bf6a44aa27c');
